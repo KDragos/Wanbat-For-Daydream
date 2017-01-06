1c40208bb7e3304c18220e9e8a4209246eb180c5
Kristin Dragos <KristinDragos@gmail.com>
2017-01-05 21:04:24 -0800

    Added some wand functionality.
    TODO: Change code to use an observer pattern to allow for more spells.
    FEATURE: Spells can now be cast based on moving the controller.
    FEATURE: Spells cast for moving the controller in the X or Y directions in
    addition to pressing the button.

---
+++ Assets/Scripts/WaitTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitTimer : MonoBehaviour {
+
+	public void StartCoroutine (){
+		StartCoroutine (UpdatePositionEverySecond ());
+	}
+
+	IEnumerator UpdatePositionEverySecond() {
+		AttackInitializer attackInitializer = FindObjectOfType<AttackInitializer>();
+		attackInitializer.previousLocation = GvrController.Orientation.eulerAngles;
+		yield return new WaitForSecondsRealtime (0.2f);
+		attackInitializer.newLocation = GvrController.Orientation.eulerAngles;
+	}
+}
